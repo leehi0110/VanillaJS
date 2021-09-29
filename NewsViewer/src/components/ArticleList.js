92a35e36038d2d9adc2a9723e2e5f9ce58722ef4
SeungNyungLee <maple0110@naver.com>
2021-09-29 17:16:32 +0900

    [NewsViewer] VanillaJS
    state를 통해 넘어온 article을 렌더링하는 컴포넌트

--- NewsViewer/src/components/ArticleList.js
+++ NewsViewer/src/components/ArticleList.js
@@ -5,16 +5,36 @@ export default function ArticleList({ $target, initialState }) {
 
   this.template = () => {
     return `
+      <div class="articlesContainer">
       ${this.state.articles
         .map(
           (article) => `
         <div class="articleContainer">
-          <p class="articleTitle">${article.title}</p>
-          <p class="articleDescription">${article.description}</p>
+          <div class="articleThumbnail">
+            ${
+              article.urlToImage === null
+                ? `<div class="articleImg emptyArticleImg">
+                  <p>이미지가 제공되지 않습니다</p>
+                </div>`
+                : `<img class="articleImg" src="${article.urlToImage}">`
+            }
+            <a class="showArticleBtn" href="${
+              article.url
+            }" target="_blank">기사 보러가기</a>
+          </div>
+          <div class="articleInfo">
+            <p class="articleTitle">${article.title}</p>
+            ${
+              article.description !== null
+                ? `<p class="articleDescription">${article.description}</p>`
+                : ""
+            }
+          </div>
         </div>
       `
         )
         .join(" ")}
+      </div>
     `;
   };
 
